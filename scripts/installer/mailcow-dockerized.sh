5bb3234758d3fc06200de535075bd3e8ff5626ef
MarvNet <marvin.kai@gmx.de>
2017-09-25 18:56:37 +0200

    Improved mailcow installer

--- scripts/installer/mailcow-dockerized.sh
+++ scripts/installer/mailcow-dockerized.sh
@@ -1,29 +1,64 @@
 cd ~
-RED='\033[0;31m'
-NC='\033[0m'
-ORANGE='\033[0;33m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
+function greenMessage {
+  echo -e "\\033[32;1m${*}\033[0m\n"
+}
 
-printf "${ORANGE}Welcome to the Mailcow-Dockerized Installer!${NC}\n"
-printf "${ORANGE}============================================${NC}\n"
-printf "${ORANGE}Wizard: (C) 2017 MarvNet${NC}\n"
-printf "${ORANGE}Mailcow: See mailcow.email${NC}\n"
-printf "${ORANGE}============================================${NC}\n"
-printf "${ORANGE}Starting...${NC}\n"
-printf "${RED}Waiting for confirmation...${NC}\n"
-pause
-printf "${GREEN}Confirmed!${NC}\n"
-printf "${ORANGE}Installing docker...${YELLOW}\n"
+function magentaMessage {
+  echo -e "\\033[35;1m${*}\033[0m\n"
+}
+
+function cyanMessage {
+  echo -e "\\033[36;1m${*}\033[0m\n"
+}
+
+function redMessage {
+  echo -e "\\033[31;1m${*}\033[0m\n"
+}
+
+function yellowMessage {
+  echo -e "\\033[33;1m${*}\033[0m\n"
+}
+
+redMessage "############################################"
+cyanMessage "Welcome to the Mailcow-Installer!        ##"
+redMessage "############################################"
+cyanMessage "Wizard: (C) 2017 MarvNet (blog.marvnet.de)#"
+redMessage "############################################"
+clear
+
+
+redMessage "10%: ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+cyanMessage "Installing docker...."
+clear
 curl -sSL https://get.docker.com/ | sh
-printf "${ORANGE}Installing docker-compose...${YELLOW}\n"
+clear
+redMessage "30%: #######~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+cyanMessage "Installing docker-compose..."
+clear
 curl -L https://github.com/docker/compose/releases/download/$(curl -Ls https://www.servercow.de/docker-compose/latest.php)/docker-compose-$(uname -s)-$(uname -m) > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-printf "${ORANGE}Downloading mailcow...${YELLOW}\n"
+clear
+redMessage "50%: ###############~~~~~~~~~~~~~~~~~~~~~~"
+cyanMessage "Installing mailcow-dockerized from GitHub..."
+clear
 git clone https://github.com/mailcow/mailcow-dockerized && cd mailcow-dockerized
-printf "${ORANGE}Generating mailcow configuration... NOTICE: User interaction required!${YELLOW}\n"
+clear
+redMessage "70%: ###########################~~~~~~~~~~"
+cyanMessage "Generating configuration..."
+clear
 ./generate_config.sh
-printf "${ORANGE}Loading mailcow and bringing it up...${YELLOW}\n"
+clear
+redMessage "99%: ##################################~~~"
+cyanMessage "Loading mailcow and bringing it up..."
+clear
 docker-compose pull
 docker-compose up -d
-printf "${GREEN}Successfully installed mailcow-dockerized! Have fun :)${NC}\n"
+clear
+redMessage   "100%: #####################################"
+greenMessage "Done!"
+greenMessage "###########################################"
+clear
+magentaMessage "Thank you for using this script :)"
+magentaMessage "DONT FORGET: To leave a comment on blog.marvnet.de/2017/09/24/mailcow-mailserver-installieren/ !!!"
+greenMessage "THANK YOU!!!!!"
+clear
